71874fc5288f0c5f264074f7f2fc76f694d228ee
Camila Pregioni Bayma <camila.bayma@gmail.com>
2024-03-15 17:07:07 -0300

    NumFinder and GHappy exercises

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,57 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class GHappyTest {
+
+    private GHappy gHappy;
+
+    @BeforeEach
+    void initialize(){
+        this.gHappy = new GHappy();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   ", "\t", "\n"})
+    void nullEmptyAndBlankStrings(String text){
+        boolean result = this.gHappy.gHappy(text);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void stringWithUnhappyG(){
+        boolean result = this.gHappy.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void stringWithHappyG(){
+        boolean result = this.gHappy.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void stringWithPairsOfHappyGs(){
+        boolean result = this.gHappy.gHappy("xxggyyggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void startsWithHappyG(){
+        boolean result = this.gHappy.gHappy("ggxxyyzz");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void endsWithHappyG() {
+        boolean result = this.gHappy.gHappy("xxyyzzgg");
+        Assertions.assertTrue(result);
+    }
+
 }
